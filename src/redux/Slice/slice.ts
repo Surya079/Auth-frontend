81c9223bbde2e87e61b9c11192a64ca56526e524
Surya079-V2 <Surya@hazhtech.com>
2024-12-01 19:44:14 +0530

    url added

--- src/redux/Slice/slice.ts
+++ src/redux/Slice/slice.ts
@@ -15,6 +15,9 @@ export const apiSlice = createApi({
   reducerPath: "apiSlice",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
+    headers: {
+      Accept: "*",
+    },
   }),
   endpoints: (builder) => ({
     register: builder.mutation({
